5b5459a57153e65fb92845808b61f43e0b7ce30b
athul-suresh-1185 <142441329+athul-suresh-1185@users.noreply.github.com>
2024-04-09 15:17:59 +0000

    Final Edit

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from .models import db, Food
-from app import db 
-
-db = SQLAlchemy()
-DB_NAME = 'database.db'
+from .extensions import db
+from flask_bcrypt import Bcrypt
 
 
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = 'some random string'
-  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'
   db.init_app(app)
+  bcrypt = Bcrypt(app)
 
 
   from .models import Admin,User,Food,MonthlyMenu,DailyMenu,Order
@@ -23,7 +21,7 @@ def create_app():
 
 def create_database(app):
   with app.app_context():
-    if not path.exists('website/'+ DB_NAME):
+    if not path.exists('website/'+ 'database.db'):
       db.create_all()
       print('Create Database!')
 
